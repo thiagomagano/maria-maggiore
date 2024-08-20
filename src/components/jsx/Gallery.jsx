131c9bd9dd59b18e2dee689f8e7e2457a5219fc8
Thiago Magano <thiagomagano1993@outlook.com>
2024-08-20 18:18:47 -0300

    galeria v0

---
+++ src/components/jsx/Gallery.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import { ColumnsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/columns.css";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import photos from "@data/fotos";
+
+export default function App() {
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <>
+      <ColumnsPhotoAlbum
+        photos={photos}
+        onClick={({ index }) => setIndex(index)}
+        columns={4}
+      />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Zoom]}
+      />
+    </>
+  );
+}
---
+++ src/data/fotos.ts
@@ -0,0 +1,23 @@
+import fachada1 from "@assets/fotos/fachada/fachada_1.webp";
+import fachada_wide from "@assets/fotos/fachada/fachada_2.webp";
+import hall1 from "@assets/fotos/area_comum/hall_1.webp";
+import hall2 from "@assets/fotos/area_comum/hall_2.webp";
+import hall3 from "@assets/fotos/area_comum/hall_3.webp";
+import bicicletario1 from "@assets/fotos/area_comum/bicicletario_1.webp";
+import bicicletario2 from "@assets/fotos/area_comum/bicicletario_2.webp";
+import salaoDeFestas1 from "@assets/fotos/area_comum/salao_de_festas.webp";
+import salaoDeFestas2 from "@assets/fotos/area_comum/salao_de_festas_2.webp";
+
+const fotos2 = [
+  fachada1,
+  fachada_wide,
+  hall1,
+  hall2,
+  hall3,
+  bicicletario1,
+  bicicletario2,
+  salaoDeFestas1,
+  salaoDeFestas2,
+];
+
+export default fotos2;
