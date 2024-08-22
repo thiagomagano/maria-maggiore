b67e20b940377870b18a4f1d98f4611262e19d39
Thiago Magano <thiagomagano1993@outlook.com>
2024-08-22 18:32:50 -0300

    section: diferenciais e localização ok

--- src/data/fotos.ts
+++ src/data/fotos.ts
@@ -4,9 +4,10 @@ import hall1 from "@assets/fotos/area_comum/hall_1.webp";
 import hall2 from "@assets/fotos/area_comum/hall_2.webp";
 import hall3 from "@assets/fotos/area_comum/hall_3.webp";
 import bicicletario1 from "@assets/fotos/area_comum/bicicletario_1.webp";
-import bicicletario2 from "@assets/fotos/area_comum/bicicletario_2.webp";
+// import bicicletario2 from "@assets/fotos/area_comum/bicicletario_2.webp";
 import salaoDeFestas1 from "@assets/fotos/area_comum/salao_de_festas.webp";
 import salaoDeFestas2 from "@assets/fotos/area_comum/salao_de_festas_2.webp";
+import cobertura from "@assets/fotos/interior/interior_801_803_cob_4.webp";
 
 const fotos2 = [
   fachada1,
@@ -15,7 +16,7 @@ const fotos2 = [
   hall2,
   hall3,
   bicicletario1,
-  bicicletario2,
+  cobertura,
   salaoDeFestas1,
   salaoDeFestas2,
 ];
