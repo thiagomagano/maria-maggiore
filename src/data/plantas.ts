b2fc1b3e5a0e5aec640e6a822662a71c79db4f36
Thiago Magano <thiagomagano1993@outlook.com>
2024-08-26 11:54:54 -0300

    wip: plantas v0

--- src/data/plantas.ts
+++ src/data/plantas.ts
@@ -1,17 +1,17 @@
-import gardens20x from "@assets/fotos/planta/Slide 12 - PB GARDENS 201,202 E 203.webp";
-import coberturas80x_1 from "@assets/fotos/planta/Slide 15 - PB COBERTURAS 801 E 803 (1PAV).webp";
-import coberturas80x_2 from "@assets/fotos/planta/Slide 15 - PB COBERTURAS 801 E 803 (2PAV).webp";
-import tipo3dorm from "@assets/fotos/planta/Slide 13 - PB TIPO 3 dorm.webp";
-import tipo2dorm from "@assets/fotos/planta/Slide 14 - PB TIPO 2dorm.webp";
-import implantacaoSS from "@assets/fotos/planta/Slide 18 - implantacao SS.webp";
-import implantacaoT from "@assets/fotos/planta/Slide 19 - implantacao T.webp";
+import gardens20x from "@assets/fotos/planta/Slide 12 - PB GARDENS 201 202 E 203.jpg";
+import coberturas80x_1 from "@assets/fotos/planta/Slide 15 - PB COBERTURAS 801 E 803 (1PAV).jpg";
+import coberturas80x_2 from "@assets/fotos/planta/Slide 15 - PB COBERTURAS 801 E 803 (2PAV).jpg";
+import tipo3dorm from "@assets/fotos/planta/Slide 13 - PB TIPO 3 dorm.jpg";
+import tipo2dorm from "@assets/fotos/planta/Slide 14 - PB TIPO 2dorm.jpg";
+import implantacaoSS from "@assets/fotos/planta/Slide 18 - implantacao SS.jpg";
+import implantacaoT from "@assets/fotos/planta/Slide 19 - implantacao T.jpg";
 
 const plantas = [
-  gardens20x,
-  coberturas80x_1,
-  coberturas80x_2,
   tipo3dorm,
   tipo2dorm,
+  coberturas80x_1,
+  coberturas80x_2,
+  gardens20x,
   implantacaoSS,
   implantacaoT,
 ];
