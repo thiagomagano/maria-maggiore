2c98b5d238333a437386723bb0117dd729e3cbb5
Thiago Magano <thiagomagano1993@outlook.com>
2024-09-06 15:00:03 -0300

    plantas antes da opiniao douglas

--- src/data/apartamentos.ts
+++ src/data/apartamentos.ts
@@ -11,6 +11,15 @@ import foto3Dorm2 from "@assets/fotos/interior/interior_tipo_3_dorm2.webp";
 
 import foto2Dorm from "@assets/fotos/interior/interior_tipo_2dorm.webp";
 
+import topgardem2 from "@assets/fotos/interior/interior_garden_201.webp";
+import topgardem1 from "@assets/fotos/interior/interior_Garden_201_203.webp";
+
+import cobertura1 from "@assets/fotos/interior/interior_801_803_cob_1.webp";
+import cobertura2 from "@assets/fotos/interior/interior_801_803_cob_2.webp";
+import cobertura3 from "@assets/fotos/interior/interior_801_803_cob_3.webp";
+import cobertura4 from "@assets/fotos/interior/interior_801_803_cob_4.webp";
+import cobertura5 from "@assets/fotos/interior/interior_801_803_cob_5.webp";
+
 interface apartamentos {
   titulo: string;
   descricao: string;
@@ -22,12 +31,21 @@ interface apartamentos {
 
 const apartamentos: apartamentos[] = [
   {
+    titulo: "Top Gardens ",
+    descricao: "(Gardens Elevados) 201, 202 e 203",
+    metragem: "118,12 a 140,62 m² privativos",
+    plantaImg: gardens20x,
+    images: [topgardem1, topgardem2],
+    colums: 3,
+  },
+
+  {
     titulo: "Apartamentos tipo 3 dormitóros",
-    descricao: "finais 01 e 03",
+    descricao: "Finais 1 e 2",
     metragem: "95,28 m² privativos",
     plantaImg: tipo3dorm,
     images: [foto3Dorm1, foto3Dorm2],
-    colums: 2,
+    colums: 3,
   },
   {
     titulo: "Apartamentos tipo 2 dormitóros",
@@ -36,6 +54,22 @@ const apartamentos: apartamentos[] = [
     plantaImg: tipo2dorm,
     images: [foto2Dorm],
     colums: 2,
+  },
+
+  {
+    titulo: "Apartamentos Cobertura",
+    descricao: "801 e 803",
+    metragem: "175,03 m² privativos",
+    plantaImg: coberturas80x_1,
+    images: [
+      coberturas80x_2,
+      cobertura1,
+      cobertura2,
+      cobertura3,
+      cobertura5,
+      cobertura4,
+    ],
+    colums: 3,
   },
 ];
 
--- src/data/fotos.ts
+++ src/data/fotos.ts
@@ -4,10 +4,9 @@ import hall1 from "@assets/fotos/area_comum/hall_1.webp";
 import hall2 from "@assets/fotos/area_comum/hall_2.webp";
 import hall3 from "@assets/fotos/area_comum/hall_3.webp";
 import bicicletario1 from "@assets/fotos/area_comum/bicicletario_1.webp";
-// import bicicletario2 from "@assets/fotos/area_comum/bicicletario_2.webp";
+import bicicletario2 from "@assets/fotos/area_comum/bicicletario_2.webp";
 import salaoDeFestas1 from "@assets/fotos/area_comum/salao_de_festas.webp";
 import salaoDeFestas2 from "@assets/fotos/area_comum/salao_de_festas_2.webp";
-import cobertura from "@assets/fotos/interior/interior_801_803_cob_4.webp";
 
 const fotos2 = [
   fachada1,
@@ -15,10 +14,10 @@ const fotos2 = [
   hall1,
   hall2,
   hall3,
-  bicicletario1,
-  cobertura,
   salaoDeFestas1,
   salaoDeFestas2,
+  bicicletario1,
+  bicicletario2,
 ];
 
 export default fotos2;
