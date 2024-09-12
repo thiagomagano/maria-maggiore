1d5c5488548ab86b301b455428c452169ba707a4
Thiago Magano <thiagomagano1993@outlook.com>
2024-09-12 14:33:25 -0300

    modificacoes planta e hero

--- src/components/jsx/Gallery.jsx
+++ src/components/jsx/Gallery.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { ColumnsPhotoAlbum } from "react-photo-album";
+import { ColumnsPhotoAlbum, RowsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/columns.css";
 
 import Lightbox from "yet-another-react-lightbox";
@@ -11,20 +11,28 @@ import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
-export default function Gallery({ photos, columns }) {
+export default function Gallery({ photos, columns, type = "columns" }) {
   const [index, setIndex] = useState(-1);
 
   return (
     <>
-      <ColumnsPhotoAlbum
-        photos={photos}
-        onClick={({ index }) => setIndex(index)}
-        columns={(containerWidth) => {
-          if (containerWidth < 400) return 1;
-          if (containerWidth < 600) return 2;
-          return columns;
-        }}
-      />
+      {type === "columns" ? (
+        <ColumnsPhotoAlbum
+          photos={photos}
+          onClick={({ index }) => setIndex(index)}
+          columns={(containerWidth) => {
+            if (containerWidth < 400) return 1;
+            if (containerWidth < 600) return 2;
+            return columns;
+            }}
+         
+        />
+      ) : (
+        <RowsPhotoAlbum
+          photos={photos}
+          onClick={({ index }) => setIndex(index)}
+        />
+      )}
 
       <Lightbox
         slides={photos}
--- src/data/apartamentos.ts
+++ src/data/apartamentos.ts
@@ -1,22 +1,22 @@
-import gardens20x from "@assets/fotos/plantas/planta_gardens.webp";
-import coberturas80x_1 from "@assets/fotos/plantas/planta_cob_pav_1.webp";
-import coberturas80x_2 from "@assets/fotos/plantas/planta_cob_pav_2.webp";
-import tipo3dorm from "@assets/fotos/plantas/planta_tipo_3_dorm.webp";
-import tipo2dorm from "@assets/fotos/plantas/planta_tipo_2_dorm.webp";
+import plantaGardens from "@assets/fotos/plantas/planta_gardens.webp";
+import plantaCoberturasPiso1 from "@assets/fotos/plantas/planta_cob_pav_1.webp";
+import plantaCoberturasPiso2 from "@assets/fotos/plantas/planta_cob_pav_2.webp";
+import plantaTipo3Dorm from "@assets/fotos/plantas/planta_tipo_3_dorm.webp";
+import plantaTipo2Dorm from "@assets/fotos/plantas/planta_tipo_2_dorm.webp";
 
-import foto3Dorm1 from "@assets/fotos/interior/interior_tipo_3dorm.webp";
-import foto3Dorm2 from "@assets/fotos/interior/interior_tipo_3_dorm2.webp";
+import interiorTipo3Dorm1 from "@assets/fotos/interior/interior_tipo_3dorm.webp";
+import interiorTipo3Dorm2 from "@assets/fotos/interior/interior_tipo_3_dorm2.webp";
 
-import foto2Dorm from "@assets/fotos/interior/interior_tipo_2dorm.webp";
+import interiorTipo2Dorm from "@assets/fotos/interior/interior_tipo_2dorm.webp";
 
-import topgardem2 from "@assets/fotos/interior/interior_garden_201.webp";
-import topgardem1 from "@assets/fotos/interior/interior_Garden_201_203.webp";
+import interiorGarden201 from "@assets/fotos/interior/interior_garden_201.webp";
+import interiorGarden201203 from "@assets/fotos/interior/interior_Garden_201_203.webp";
 
-import cobertura1 from "@assets/fotos/interior/interior_801_803_cob_1.webp";
-import cobertura2 from "@assets/fotos/interior/interior_801_803_cob_2.webp";
-import cobertura3 from "@assets/fotos/interior/interior_801_803_cob_3.webp";
-import cobertura4 from "@assets/fotos/interior/interior_801_803_cob_4.webp";
-import cobertura5 from "@assets/fotos/interior/interior_801_803_cob_5.webp";
+import interiorCobertura801803_1 from "@assets/fotos/interior/interior_801_803_cob_1.webp";
+import interiorCobertura801803_2 from "@assets/fotos/interior/interior_801_803_cob_2.webp";
+import interiorCobertura801803_3 from "@assets/fotos/interior/interior_801_803_cob_3.webp";
+import interiorCobertura801803_4 from "@assets/fotos/interior/interior_801_803_cob_4.webp";
+import interiorCobertura801803_5 from "@assets/fotos/interior/interior_801_803_cob_5.webp";
 
 interface apartamentos {
   titulo: string;
@@ -29,46 +29,48 @@ interface apartamentos {
 
 const apartamentos: apartamentos[] = [
   {
+    titulo: "Apartamentos Cobertura",
+    descricao: "(801 e 803)",
+    metragem: "175,03 m² privativos",
+    plantaImg: plantaCoberturasPiso1,
+    images: [
+      plantaCoberturasPiso2,
+      interiorCobertura801803_1,
+      interiorCobertura801803_2,
+      interiorCobertura801803_3,
+      interiorCobertura801803_5,
+      interiorCobertura801803_4,
+    ],
+    colums: 4,
+  },
+  
+  {
     titulo: "Top Gardens ",
-    descricao: "(Gardens Elevados) 201, 202 e 203",
+    descricao: "Gardens Elevados (201, 202 e 203)",
     metragem: "118,12 a 140,62 m² privativos",
-    plantaImg: gardens20x,
-    images: [topgardem1, topgardem2],
+    plantaImg: plantaGardens,
+    images: [interiorGarden201203, interiorGarden201],
     colums: 3,
   },
 
   {
-    titulo: "Apartamentos tipo 3 dormitóros",
-    descricao: "Finais 1 e 2",
+    titulo: "Apartamentos Tipo 3 dormitórios",
+    descricao: "(Finais 01 e 02)",
     metragem: "95,28 m² privativos",
-    plantaImg: tipo3dorm,
-    images: [foto3Dorm1, foto3Dorm2],
+    plantaImg: plantaTipo3Dorm,
+    images: [interiorTipo3Dorm1, interiorTipo3Dorm2],
     colums: 3,
   },
   {
-    titulo: "Apartamentos tipo 2 dormitóros",
-    descricao: "final 2",
+    titulo: "Apartamentos Tipo 2 dormitórios",
+    descricao: "(Final 02)",
     metragem: "66,94 m² privativos",
-    plantaImg: tipo2dorm,
-    images: [foto2Dorm],
+    plantaImg: plantaTipo2Dorm,
+    images: [interiorTipo2Dorm],
     colums: 2,
   },
 
-  {
-    titulo: "Apartamentos Cobertura",
-    descricao: "801 e 803",
-    metragem: "175,03 m² privativos",
-    plantaImg: coberturas80x_1,
-    images: [
-      coberturas80x_2,
-      cobertura1,
-      cobertura2,
-      cobertura3,
-      cobertura5,
-      cobertura4,
-    ],
-    colums: 3,
-  },
+  
 ];
 
 export default apartamentos;
