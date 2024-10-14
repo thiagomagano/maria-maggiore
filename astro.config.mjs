e168e2983fe913ee82cab07d212c72f99d31de45
Thiago Magano <thiagomagano1993@outlook.com>
2024-10-14 11:45:57 -0300

    imageCDN: FALSE

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: netlify({
+    imageCDN: false
+  }),
   integrations: [tailwind(), react()],
 });
